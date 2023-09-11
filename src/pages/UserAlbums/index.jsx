6daf5893dd9f1115b89976ff7b5f34fc4ee1bd56
Anton Gerasimov <gerasymov44@gmail.com>
2023-09-11 12:41:12 +0300

    develop user list

---
+++ src/pages/UserAlbums/index.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { Header } from '../../components/Header'
+import axios from 'axios'
+import styles from './styles.module.scss'
+import { useParams } from 'react-router-dom'
+
+export const UserAlbums = () => {
+  const params = useParams()
+  const [userAlbums, setUserAlbums] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${params.id}/albums`)
+      .then((data) => {
+        console.log(data)
+        setUserAlbums(data?.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <Header title={`${params.name} albums`} />
+      <div className={styles.userAlbums}>
+        {userAlbums.map((i) => (
+          <div key={i.id} className={styles.album}>
+            <h2 className={styles.album__title}>{i.title}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
